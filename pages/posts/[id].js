6aabb78f0ce24a5be03d6993dc4f34e7033b0e81
malujie <17862835855@163.com>
2024-05-07 17:04:08 +0800

    ✨tailwindcss

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -11,7 +11,7 @@ export default function Post({ postData }) {
         <title>{postData.title}</title>
       </Head>
       <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <h1  className="text-teal-600 text-3xl font-bold mb-4">{postData.title}</h1>
         <div className={utilStyles.lightText}>
           <Date dateString={postData.date} />
         </div>
